bd6939849fad4895a2b83b20e486776bddf4c8eb
sigristarisa <arisasigrist.ch@gmail.com>
2023-08-07 11:50:38 +0200

    added search bar

--- src/Components/TableTitle.jsx
+++ src/Components/TableTitle.jsx
@@ -1,28 +1,57 @@
+import { useState, useEffect } from "react";
 import "../styles/TableTitle.css";
 
 const TableTitle = ({ openFilter, setOpenFilter }) => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+
+  const updateScreenSize = () => setIsDesktop(window.innerWidth > 768);
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenSize);
+    console.log("is desktop", isDesktop);
+  });
   return (
-    <div className='container__table-title'>
-      <h1>Your Inventory</h1>
-      <div>
-        <input type='text' />
-      </div>
-      <div
-        className='container__filter-icon'
-        onClick={() => setOpenFilter(!openFilter)}
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 -4 32 40'
-          x='0px'
-          y='0px'
-          className='icon__filter'
+    <div className='container__table-title two-grid-column__extend-two'>
+      <aside className='container__table-title--left'>
+        <div>
+          <h1>Your Inventory</h1>
+        </div>
+      </aside>
+      <aside className='container__table-title--right two-grid-column__extend-one '>
+        {isDesktop && (
+          <div className='container__input-search'>
+            <input
+              type='text'
+              className='input__search'
+              placeholder='Search product or user'
+            />
+          </div>
+        )}
+        <div
+          className='container__filter-icon'
+          onClick={() => setOpenFilter(!openFilter)}
         >
-          <path d='M3,8h15.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h3.142c.553,0,1-.448,1-1s-.447-1-1-1h-3.142c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1Zm19-3c1.103,0,2,.897,2,2s-.897,2-2,2-2-.897-2-2,.897-2,2-2Z' />
-          <path d='M29,15H13.858c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1h3.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h15.142c.553,0,1-.448,1-1s-.447-1-1-1Zm-19,3c-1.103,0-2-.897-2-2s.897-2,2-2,2,.897,2,2-.897,2-2,2Z' />
-          <path d='M29,24h-3.142c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1h15.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h3.142c.553,0,1-.448,1-1s-.447-1-1-1Zm-7,3c-1.103,0-2-.897-2-2s.897-2,2-2,2,.897,2,2-.897,2-2,2Z' />
-        </svg>
-      </div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 -4 32 40'
+            x='0px'
+            y='0px'
+            className='icon__filter'
+          >
+            <path d='M3,8h15.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h3.142c.553,0,1-.448,1-1s-.447-1-1-1h-3.142c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1Zm19-3c1.103,0,2,.897,2,2s-.897,2-2,2-2-.897-2-2,.897-2,2-2Z' />
+            <path d='M29,15H13.858c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1h3.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h15.142c.553,0,1-.448,1-1s-.447-1-1-1Zm-19,3c-1.103,0-2-.897-2-2s.897-2,2-2,2,.897,2,2-.897,2-2,2Z' />
+            <path d='M29,24h-3.142c-.447-1.72-1.999-3-3.858-3s-3.411,1.28-3.858,3H3c-.553,0-1,.448-1,1s.447,1,1,1h15.142c.447,1.72,1.999,3,3.858,3s3.411-1.28,3.858-3h3.142c.553,0,1-.448,1-1s-.447-1-1-1Zm-7,3c-1.103,0-2-.897-2-2s.897-2,2-2,2,.897,2,2-.897,2-2,2Z' />
+          </svg>
+        </div>
+      </aside>
+      {!isDesktop && (
+        <div className='container__input-search'>
+          <input
+            type='text'
+            className='input__search'
+            placeholder='Search product or user'
+          />
+        </div>
+      )}
     </div>
   );
 };
